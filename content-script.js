c93fb1e7963862f13abf66c2dcdd61fd6ea79a02
Anton Tarasov <anton.tarasov@quantumsoft.ru>
2022-03-16 01:39:45 +0700

    Add first virsion of wordhunt extension

---
+++ content-script.js
@@ -0,0 +1,85 @@
+(() => {
+  let isCtrlPressed = false;
+  let isMouseUp = true;
+  let scheduleAfterMouseUp = false;
+
+  const findWord = () => {
+    if (!isCtrlPressed) {
+      return;
+    }
+    if (!isMouseUp) {
+      scheduleAfterMouseUp = true;
+      return;
+    }
+    const selection = window.getSelection();
+    if (!selection || selection.rangesCount == 0) {
+      return;
+    }
+    let text = selection.getRangeAt(0).toString();
+    if (text) {
+      openModal(text.trim());
+    }
+  };
+
+  const getModal = () => {
+    return document.getElementById("wh-modal");
+  };
+
+  const openModal = (text) => {
+    if (getModal()) {
+      return;
+    }
+    modal = document.createElement("div");
+    modal.setAttribute("id", "wh-modal");
+    modal.addEventListener("click", deleteModal);
+
+    const content = document.createElement("div");
+    content.setAttribute("id", "wh-modal-content");
+
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute("id", "wh-iframe");
+    iframe.setAttribute("src", "https://paragraph77.net/word/" + text);
+
+    content.appendChild(iframe);
+    modal.appendChild(content);
+    document.body.appendChild(modal);
+  };
+
+  const deleteModal = () => {
+    const modal = getModal();
+    if (modal) {
+      modal.remove();
+    }
+  };
+
+  document.addEventListener("selectionchange", () => {
+    findWord();
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey) {
+      isCtrlPressed = true;
+    }
+    if (e.key == "Escape") {
+      deleteModal();
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (!e.ctrlKey) {
+      isCtrlPressed = false;
+    }
+  });
+
+  document.addEventListener("mousedown", () => {
+    isMouseUp = false;
+  });
+
+  document.addEventListener("mouseup", () => {
+    isMouseUp = true;
+    if (scheduleAfterMouseUp) {
+      scheduleAfterMouseUp = false;
+      findWord();
+    }
+  });
+})();
